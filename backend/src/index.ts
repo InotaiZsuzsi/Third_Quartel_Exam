335513996412dd7c5065318fba3d6d6a65f2db26
InotaiZsuzsi <inotai.zsuzsanna@gmail.com>
2024-03-01 18:33:30 +0100

    primitiv_server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,15 @@
-"use strict";
-console.log("Hello World!")
+import http from "http"
+
+function demo() {
+    console.log("demo")
+}
+
+const server = http.createServer((req, res) => {
+    demo();
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({
+      data: 'Hello World!',
+    }));
+  });
+
+  server.listen(4213)
